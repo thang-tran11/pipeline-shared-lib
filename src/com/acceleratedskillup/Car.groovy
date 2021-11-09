773c811bdc3404347d79e621a0ca091c96c9bd6e
thang-tran11 <83936306+thang-tran11@users.noreply.github.com>
2021-11-09 18:06:12 +0700

    Update Car.groovy

--- src/com/acceleratedskillup/Car.groovy
+++ src/com/acceleratedskillup/Car.groovy
@@ -4,10 +4,12 @@ class Car {
   int numberOfDoors
   String brand
   String model
+  String a
   def script
   
 	Car(script) {
 		this.script = script
+		this.a = script.getClass()
 	}
 	
   	void printOutCar() {
@@ -15,6 +17,7 @@ class Car {
   		script.echo "numberOfDoors:${numberOfDoors}"    	
 		script.echo "brand:${brand}"    	
 		script.echo "model:${model}"
+		script.echo "a:${a}"
   	}  
   
 }
